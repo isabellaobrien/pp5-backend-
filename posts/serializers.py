2109c134cdd574cb0ab3ad4a56a121d627435edc
isabellaobrien <bellaobrien.it@gmail.com>
2023-12-24 21:03:21 +0000

    Add edit and delete functionality to PostDetail view

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,16 +2,15 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    # owner = serializers.ReadOnlyField(source='owner.username')
-    # is_owner = serializers.SerializerMethodField()
-    # profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
 
-    # def get_is_owner(self, obj):
-    #     request = self.context['request']
-    #     return request.user == obj.owner
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'title','image', 'excerpt', 'content', 'status', 
+            'id', 'owner', 'created_at', 'updated_at', 'title','image', 'excerpt', 'content', 'status', 'is_owner' 
         ]
