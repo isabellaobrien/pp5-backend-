dd17c46c76d9188c04f7efca1bd9cc4a762bb06a
isabellaobrien <bellaobrien.it@gmail.com>
2023-12-25 19:39:40 +0000

    Add comments app and CreateList view

---
+++ comments/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics, permissions
+from .serializers import CommentSerializer
+from .models import Comment
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
